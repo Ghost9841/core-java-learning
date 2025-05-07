491fae394286c5f31ee15b25ee46099162c53b0b
Ghost9841 <asubedi778@gmail.com>
2025-05-07 09:45:27 +0545

    Added Chapter 1

--- Welcome.java
+++
@@ -1,3 +0,0 @@
-public class Welcome{
-    public 
-}
---
+++ chapter 1/Welcome.java
@@ -0,0 +1,12 @@
+// Listing 1.1
+
+// Listing 1.1 Welcome.java
+
+public class Welcome{
+    public static void main (String[] args){
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++) 
+            System.out.print("=");
+    }
+}
