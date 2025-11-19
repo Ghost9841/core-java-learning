e5c805a90e247327195b60e7c39ac17762f4dfdb
Ghost9841 <asubedi778@gmail.com>
2025-11-19 08:03:43 +0545

    Operation java developer

---
+++ class-clg/lecture1/Operation.java
@@ -0,0 +1,9 @@
+public class Operation {
+
+    public static void main(String[] args) {
+        int result1 = (6+2)*3;
+        int result2 = 6+(2*3);
+        System.out.println("Result1: " + result1);
+        System.out.println("Result2: " + result2);
+    }
+}
