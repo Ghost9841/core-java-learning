5b57ca1e350a87ab6089c0a96b5f90db372dcea0
Ghost9841 <asubedi778@gmail.com>
2025-11-26 17:25:31 +0545

    Task1

---
+++ class-clg/lecture2/Task1.java
@@ -0,0 +1,8 @@
+public class Task1{
+    public static void main(String[] args) {
+        String[] names = {"A","B"};
+        for(int i=0; i< names.length; i++){
+            System.out.println(names[i]);
+        }
+    }
+}
